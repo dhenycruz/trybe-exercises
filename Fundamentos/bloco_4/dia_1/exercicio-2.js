50c91c9943aca1adec6bf54d0c1d29b12005c736
Dheniarley Cruz <dheniarley.ds@gmail.com>
2021-06-24 18:27:27 -0300

    Exercício 2.

---
+++ Fundamentos/bloco_4/dia_1/exercicio-2.js
@@ -0,0 +1,8 @@
+let numero1 = 5;
+let numero2= 9;
+
+if(numero1 > numero2){
+    console.log('O maior número entre '+numero1+' e '+numero2+' é '+numero1);
+} else {
+    console.log('O maior número entre '+numero1+' e '+numero2+' é '+numero2);
+}
