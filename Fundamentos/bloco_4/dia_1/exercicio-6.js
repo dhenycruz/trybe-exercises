129726704b7de4fb1fddee3c3d83b480f59ce571
Dheniarley Cruz <dheniarley.ds@gmail.com>
2021-06-25 19:21:38 -0300

    Enviando exercício 6 Bloco 4 - dia 1.

--- Fundamentos/bloco_4/dia_1/exercicio-6.js
+++ Fundamentos/bloco_4/dia_1/exercicio-6.js
@@ -15,3 +15,28 @@
  * 
  */
 
+let pecaXadrex = "Peao";
+
+switch (pecaXadrex){
+    case 'Peao':
+        console.log('Peão -> O Peão pode se mover uma casa para frente ou duas no seu primeiro movimento e para atacar uma casa para diagonais na sua frente.');
+        break;
+    case 'Torre':
+        console.log('Torre ->A Torre pode se mover na vertical ou horizontal quantas casa quiser.');
+        break;
+    case 'Cavalo':
+        console.log('Cavalo -> O cavalo pode se mover em L, a base do L duas casas e 3 casas na vertical do L');
+        break;
+    case 'Bispo':
+        console.log('Bispo -> O Bispo pode se mover nas diagonais e para todos os sentindos do tabuleiro.');
+        break;
+    case 'Rainha':
+        console.log('Rainha -> A Rainha pode se mover na vertical, horizontal, em todas horizontais em qualquer sentindo.');
+        break;
+    case 'Rei':
+        console.log('Rei -> O rei pode se mover uma casa para qualquer sentindo, tanto para se mover, quanto para atacar');
+        break;
+    default:
+        console.log('Não foi possivep identificar a peça que você está se referindo.');
+        break;    
+}
