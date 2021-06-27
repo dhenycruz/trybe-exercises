55e1bcba8252c3badbfe39acbdaf6f46e1a5d45a
Dheniarley Cruz <dheniarley.ds@gmail.com>
2021-06-27 00:03:04 -0300

    Exercício 8 e 9.

---
+++ Fundamentos/bloco_4/dia_1/exercício-9.js
@@ -0,0 +1,19 @@
+/**
+ * Escreva um programa que defina três números em variáveis e ,
+ * retorne true se pelo menos uma das três for ímpar. Caso contrário, 
+ * ele retorna false .
+ * 
+ *  - Bonus: use somente um if .
+ * 
+ * */
+
+ let num1 = 3;
+ let num2 = 5;
+ let num3 = 7;
+ 
+ if (num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0) {
+     console.log(true);
+ } else {
+     console.log(false);
+ }
+ 
