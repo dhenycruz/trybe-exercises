d33dd7ba1f8d3ec0257be408aa2decc5624f304f
Dheniarley Cruz <dheniarley.ds@gmail.com>
2021-06-28 18:59:08 -0300

    Exercício 1 - bonus.

---
+++ Fundamentos/bloco_4/dia_2/bonus/exericicio-1.js
@@ -0,0 +1,9 @@
+// Ordene o array numbers em ordem crescente e imprima seus valores;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (index = 0; index < numbers.length; index += 1) {
+    for (index2 = 0; index2 <numbers.length; index2 += 1){
+
+    }
+}
