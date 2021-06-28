97b76ff07a81565771c06b80317355ec703b2722
Dheniarley Cruz <dheniarley.ds@gmail.com>
2021-06-28 13:07:50 -0300

    Exercício 5.js

---
+++ Fundamentos/bloco_4/dia_2/exercicio-5.js
@@ -0,0 +1,12 @@
+//Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorValor = 0;
+
+for (index = 0; index < numbers.length; index += 1) {
+    if(numbers[index] > maiorValor){
+        maiorValor = numbers[index];
+    }
+}
+
+console.log(maiorValor);
