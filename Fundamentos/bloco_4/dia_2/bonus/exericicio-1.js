733f5dba211a0b8a67db302d689d8c7326d688c9
Dheniarley Cruz <dheniarley.ds@gmail.com>
2021-06-28 19:03:20 -0300

    Exercício 1 - Bônus, realizado.

--- Fundamentos/bloco_4/dia_2/bonus/exericicio-1.js
+++ Fundamentos/bloco_4/dia_2/bonus/exericicio-1.js
@@ -2,8 +2,17 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (index = 0; index < numbers.length; index += 1) {
-    for (index2 = 0; index2 <numbers.length; index2 += 1){
+console.log(numbers);
+
+for (let index = 1; index < numbers.length; index += 1) {
+    for (let index2 = 0; index2 <numbers.length; index2 += 1){
+        if(numbers[index] < numbers[index2]){
+            let position = numbers[index];
+            numbers[index] = numbers[index2];
+            numbers[index2] = position;
+        }
 
     }
 }
+
+console.log(numbers);
