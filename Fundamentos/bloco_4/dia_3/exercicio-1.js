d926039a42a8cd475200378fe40a0b6699c8c3f7
dhenycruz <dheniarley.ds@gmail.com>
2021-07-19 21:37:38 -0300

    Enviando exercícios Objetos e funções.

--- Fundamentos/bloco_4/dia_3/exercicio-1.js
+++ Fundamentos/bloco_4/dia_3/exercicio-1.js
@@ -1,10 +1,11 @@
 // 1. Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
 let n = 5;
-let lines;
 let Columns = '';
 
 for (let nColumns = 1; nColumns <= n; nColumns += 1) {
     Columns += '*';
 }
+
+for (let lines = 1; lines <= n; )
 
 console.log(Columns);
